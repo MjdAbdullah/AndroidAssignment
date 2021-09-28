c3d9b46f12241dcd4411c9cafe57f99571096b71
mjd <mjd.abdullah75@gmail.com>
2021-09-29 01:32:29 +0300

    Create RecyclerViewAdapter.kt

---
+++ NumbersGameApp/NumbersGameApp/app/src/main/java/com/example/numbersgameapp/RecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.numbersgameapp
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_row.view.*
+import android.content.Context
+
+
+class RecyclerViewAdapter (val context: Context, val arr : ArrayList<String>): RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>(){
+    class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val data = arr[position]
+        holder.itemView.apply {
+           tvColor.text = data
+        }
+    }
+
+    override fun getItemCount() = arr.size
+}
